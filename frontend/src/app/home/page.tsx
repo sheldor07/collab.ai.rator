41bf2755831e5abcb8305dc4c6c336304f104aa4
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-06-09 14:21:26 +0530

    homepage done, timeline timepoint allignment issue
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

---
+++ frontend/src/app/home/page.tsx
@@ -0,0 +1,15 @@
+import Timeline from "../../components/Timeline"
+export default function Page(){
+    return (<div className="flex flex-col h-screen bg-white">
+        <div className="py-10 text-center">
+            <p className="text-sm font-light text-violet-400"> THE ULTIMATE CC0001 WRITING TOOL</p>
+            <h1 className="py-4 text-5xl font-black text-black"> Welcome to Collab.AI.tor</h1>
+            <p className="w-full text-sm text-slate-400"> A workspace to help you brainstorm ideas and write better with an AI assistant.</p>
+        </div>
+        <div className="flex flex-col w-2/5 px-10 text-left">
+            <h2 className="text-3xl text-black underline">Roadmap</h2>
+            <p className="text-lg text-black whitespace-pre-line">You will go through each design thinking stages that will guide you to write a persuavsive Op-Ed</p>
+        </div>
+        <Timeline/>
+    </div>)
+}
